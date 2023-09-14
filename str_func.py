28cc6108e92969975566a03699904bd7df90a98d
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-09-14 12:05:40 +0300

    add title_string()

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def uppercase_string(string:str):
     return string.upper()
+
+
+def title_string(string:str):
+    """
+    Делает заглавными первые буквы каждого слова в строке
+    """
+    return string.title()
